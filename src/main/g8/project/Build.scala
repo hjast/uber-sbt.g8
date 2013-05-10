1ab52faac0c268858480689203f29eacd1ab5557
Reuben Doetsch <reuben.doetsch@gmail.com>
2013-05-10 10:41:56 -0400

    Fixed the console string bug

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -55,7 +55,7 @@ object $name$Build extends Build {
   
   //Change this to change your custom console for more imports. 
   //Anthing added to console will be here automatically 
-  val initialConsole: String = """
+  val initialConsoleString: String = """
   	println("Welcome to the utility console")
 	Console.init
 	import Console._
